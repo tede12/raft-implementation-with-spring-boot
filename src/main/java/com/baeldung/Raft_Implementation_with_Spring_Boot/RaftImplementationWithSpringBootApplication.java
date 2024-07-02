574766d5c00c696eca63f8ebe1a4191de365acbf
tede12 <tedeschini.marco@gmail.com>
2024-07-02 10:49:20 +0200

    Initial commit

---
+++ src/main/java/com/baeldung/Raft_Implementation_with_Spring_Boot/RaftImplementationWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.Raft_Implementation_with_Spring_Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaftImplementationWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaftImplementationWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baeldung/Raft_Implementation_with_Spring_Boot/RaftImplementationWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.Raft_Implementation_with_Spring_Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaftImplementationWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
