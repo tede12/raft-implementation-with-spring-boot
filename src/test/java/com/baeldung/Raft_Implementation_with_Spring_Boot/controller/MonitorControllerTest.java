9ae1bb31e7bbed667e0762b1553d8ba392851444
tede12 <tedeschini.marco@gmail.com>
2024-08-10 23:49:46 +0200

    Added test for monitor controller

---
+++ src/test/java/com/baeldung/Raft_Implementation_with_Spring_Boot/controller/MonitorControllerTest.java
@@ -0,0 +1,28 @@
+package com.baeldung.Raft_Implementation_with_Spring_Boot.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@WebFluxTest(controllers = MonitorController.class)
+class MonitorControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void testMonitorPage_Success() {
+        webTestClient.get()
+                .uri("/monitor")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentTypeCompatibleWith("text/html")
+                .expectBody(String.class)
+                .consumeWith(response -> {
+                    String body = response.getResponseBody();
+                    assert body != null;
+                    assert body.contains("Raft Node Status");
+                });
+    }
+}
