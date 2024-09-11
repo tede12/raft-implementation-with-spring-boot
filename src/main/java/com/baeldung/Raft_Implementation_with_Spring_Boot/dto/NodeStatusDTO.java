5ef55ba6331ac7e7e4ff292dd66d22c6d4c543a6
tede12 <tedeschini.marco@gmail.com>
2024-09-11 22:52:37 +0200

    Imported right version of SpringDoc OpenAPI and added openapi to NodeStateEntity, NodeState, NodeStatusDTO

--- src/main/java/com/baeldung/Raft_Implementation_with_Spring_Boot/dto/NodeStatusDTO.java
+++ src/main/java/com/baeldung/Raft_Implementation_with_Spring_Boot/dto/NodeStatusDTO.java
@@ -1,16 +1,26 @@
 package com.baeldung.Raft_Implementation_with_Spring_Boot.dto;
 
 import com.baeldung.Raft_Implementation_with_Spring_Boot.persistence.model.NodeState;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class NodeStatusDTO {
+    @Schema(description = "Unique identifier of the node", example = "node1")
     private String nodeId;
+
+    @Schema(description = "Current state of the node in the Raft cluster", example = "FOLLOWER")
     private NodeState state;
+
+    @Schema(description = "Current term number of the node", example = "1")
     private int currentTerm;
+
+    @Schema(description = "ID of the node this node has voted for in the current term", example = "node2")
     private String votedFor;
+
+    @Schema(description = "URL of the node", example = "localhost:8000")
     private String nodeUrl;
 
     public NodeStatusDTO(String nodeId, NodeState state, int currentTerm, String votedFor, String nodeUrl) {
--- src/main/java/com/baeldung/Raft_Implementation_with_Spring_Boot/persistence/model/NodeStateEntity.java
+++ src/main/java/com/baeldung/Raft_Implementation_with_Spring_Boot/persistence/model/NodeStateEntity.java
@@ -1,5 +1,6 @@
 package com.baeldung.Raft_Implementation_with_Spring_Boot.persistence.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.EqualsAndHashCode;
@@ -16,19 +17,24 @@ import org.springframework.data.relational.core.mapping.Column;
 @EqualsAndHashCode
 public class NodeStateEntity {
     @Id
+    @Schema(description = "Unique identifier of the record", example = "1")
     private Long id;
 
     @Column("node_id")
+    @Schema(description = "Unique identifier of the node", example = "node1")
     private String nodeId;
 
     @Enumerated(EnumType.STRING)
     @Column("state")
+    @Schema(description = "Current state of the node", example = "FOLLOWER")
     private NodeState state;
 
     @Column("current_term")
+    @Schema(description = "Current term number", example = "1")
     private int currentTerm;
 
     @Column("voted_for")
+    @Schema(description = "Node ID that this node has voted for", example = "node2")
     private String votedFor;
 
     @Override
