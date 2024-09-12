a93eb96a8cd303f1d00e9ce0cad0eba77e96302c
tede12 <tedeschini.marco@gmail.com>
2024-09-12 10:13:27 +0200

    Added OpenAPI to GlobalExceptionHandler

--- src/main/java/com/baeldung/Raft_Implementation_with_Spring_Boot/exception/GlobalExceptionHandler.java
+++ src/main/java/com/baeldung/Raft_Implementation_with_Spring_Boot/exception/GlobalExceptionHandler.java
@@ -6,11 +6,27 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import reactor.core.publisher.Mono;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NodeStateNotFoundException.class)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Node state not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     public Mono<ResponseEntity<String>> handleNodeStateNotFoundException(NodeStateNotFoundException ex) {
         return Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage()));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public Mono<ResponseEntity<String>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage()));
     }
 }
